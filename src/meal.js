68fd8adc70fa816661588165b0691f5d1c89f1d7
Chris Butler <butlertree@gmail.com>
2023-08-24 21:19:50 -0600

    "Adds functions to satisfy tests"

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
-hello
+
+function createMenuItem (menuName, price, type) {
+  return {
+  name: menuName,
+  price: price,
+  type:  type
+}
+}
+
+function addIngredients(ingredient, ingredientArray){
+  if(!ingredientArray.includes(ingredient)) {
+     ingredientArray.push(ingredient);
+  } return ingredientArray
+}
+ 
+  function formatPrice (formatPrice) {
+     return `$${formatPrice}`
+  }
+
+function decreasePrice (price){
+return price * .9
+}
+
+function createRecipe (title, ingredients, type)  {
+return {
+  title: title,
+  ingredients: ingredients,
+  type: type
+}
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
